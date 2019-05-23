7649607a1c0a1e5591870e329411a8155b6ae020
AkshayaElangovan <50879320+AkshayaElangovan@users.noreply.github.com>
2019-05-23 14:35:36 +0530

    Create vocon.py

---
+++ vocon.py
@@ -0,0 +1,5 @@
+akshu=input()
+if(akshu=='a'or akshu=='e'or akshu=='i'or akshu='o'or akshu=='u'or akshu=='A'or akshu=='E'or akshu=='I'or akshu=='O'or akshu=='U'):
+   print("Vowel")
+else:
+   print("Consonant")
